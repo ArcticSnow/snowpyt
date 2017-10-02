3035f6861e909da39bc5fc1eab2a6b4054d028a1
arcticsnow <svfilhol@alaska.edu>
2017-10-02 14:33:33 +0200

    version 1.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.3',
+    version='0.1.4',
 
     description='A Python project to visualize snowpit',
     long_description=long_description,
@@ -67,7 +67,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['matplotlib', 'pandas', 'numpy', 'xlrd', 'xml'],
+    install_requires=['matplotlib', 'pandas', 'numpy', 'xlrd'],
 
     include_package_data=True
 
